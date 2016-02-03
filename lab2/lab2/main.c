f4bfd9d2bce1792019f382ca581805aebe8a8b36
Rup Gautam <rup.gautam@yahoo.com>
2016-02-02 23:53:49 -0500

    lab2 added and it's source

---
+++ lab2/lab2/main.c
@@ -0,0 +1,67 @@
+//
+//  main.c
+//  IPC Lab_2
+//
+//  Created by Rup Gautam on 2016-01-27.
+//  Copyright @ 2016 Rup Gautam. All rights reserved.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+
+
+{
+    int strength;
+    int speed;
+    int defense;
+    int intel;
+    float sum;
+    float ratio_strength;
+    float ratio_speed;
+    float ratio_defense;
+    float ratio_intel;
+
+
+// user inputs
+
+    printf("Strength:?\n");
+    scanf("%d", &strength);
+
+    printf("Speed:?\n");
+    scanf("%d", &speed);
+
+    printf("Defense:?\n");
+    scanf("%d", &defense);
+
+    printf("Intel:?\n");
+    scanf("%d", &intel);
+
+
+// sum of all the attributes
+    sum = (strength + speed + defense + intel);
+
+
+// dividing and multiply to get ratio
+    ratio_strength = (strength / sum) * 100;
+    ratio_speed = (speed / sum) * 100;
+    ratio_defense = (defense / sum) * 100;
+    ratio_intel = ((intel / sum) * 100);
+
+
+
+// output the final stats result
+
+    printf("Your player's final stats are\n");
+
+    printf("Strength: %d\n", (int)ratio_strength);
+    printf("Speed: %d\n", (int)ratio_speed);
+    printf("Defense: %d\n", (int)ratio_defense);
+    printf("Intelligence: %d\n", (int)ratio_intel);
+
+
+
+    return 0;
+
+}
