b94c7a9067ed28b63e948370f7405ef9527e0eef
Rup Gautam <rup.gautam@yahoo.com>
2016-04-06 10:26:18 -0400

    perfectly working lab 8

--- lab8/lab8/main.c
+++ lab8/lab8/main.c
@@ -11,9 +11,27 @@
 // or distributed to other students.
 // Name   Rup Gautam   Student ID  033838152
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+
+
 
 int main(){
 
-	printf("Hello This is Lab_8");
+	char s[100];
+	int count = 0, i;
+
+	printf("Word Counter\n ===============\n\n");
+	printf("Text to be analyzed\n");
+	fgets(s, 100, stdin);
+
+
+	for (i = 0; s[i] != '\0'; i++){
+		if (s[i] == ' ' && s[i + 1] != ' ');
+		count++;
+	}
+
+	printf("Word count: %d\n", count + 1);
+	return 0;
 }
